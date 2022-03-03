cbc1dac5960da11052f28856ef2cb0c8d7aec582
Huseyin San <hsan@eidvdi4010.eu.asml.com>
2022-03-03 11:29:01 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# asml-setup-go
-Action to setup Go using Artifacatory
+# ASML Setup Go
+
+A GitHub Action to setup your workflow to use a specific version of Go fetched from [Artifactory](https://artifactory-de.asml.com/ui/repos/tree/General/rise-generic-dev-local/toolchain-cache).
+
+```yaml
+- uses: asml-setup-go
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+
+const token = "X-JFrog-Art-Api:"+process.env.ARTIFACTORY_TOKEN 
+
+const main = async () => {
+  try {
+
+    await exec.exec('curl', ["-H",token,"-O","https://artifactory-de.asml.com/artifactory/rise-generic-dev-local/toolchain-cache/go_tool_cache.tar.gz"]);
+
+    await exec.exec("tar", ["-zxf", "go_tool_cache.tar.gz"]);
+    await exec.exec("cp", ["-r", "go", "/opt/hostedtoolcache"]);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+  try {
+    let version = core.getInput('go-version');
+    await exec.exec("echo", [version]);
+
+    switch (version.slice(0,4)) {
+      case "1.15":
+        core.addPath("/opt/hostedtoolcache/go/1.15.15/x64/bin");
+        break;
+      case "1.16":
+        core.addPath("/opt/hostedtoolcache/go/1.16.14/x64/bin");
+        break;
+      case "1.17":
+        core.addPath("/opt/hostedtoolcache/go/1.17.7/x64/bin")
+        break;
+      default:
+        core.setFailed("go-version needs to be specified !! (SUPPORTED VERSIONS:1.15.x, 1.16.x, 1.17.x)");
+        break;
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+// Call the main function to run the action
+main();
