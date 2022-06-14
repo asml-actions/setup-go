7185e95d37d4b7eb3a3148783a397dc9b2dcdfa9
Huseyin San <hsan@eidvdi4010.eu.asml.com>
2022-06-14 15:47:06 +0200

    Dynamic Path

--- dist/index.js
+++ dist/index.js
@@ -2969,7 +2969,6 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(186);
 const exec = __nccwpck_require__(514);
-<<<<<<< HEAD
 const fs = __nccwpck_require__(147);
 
 function getDirectories(path) {
@@ -2982,8 +2981,7 @@ const token = "Authorization: Bearer " + process.env.ARTIFACTORY_TOKEN;
 const toolsPath = process.env.RUNNER_TOOL_CACHE;
 const artifactoryURL =
   "https://artifactory-de.asml.com/artifactory/rise-generic-dev-local/toolchain-cache/go_tool_cache.tar.gz";
-const version = core.getInput('go-version');
-
+const version = core.getInput("go-version");
 
 if (!process.env.ARTIFACTORY_TOKEN) {
   core.setFailed(
@@ -2994,28 +2992,11 @@ if (!process.env.ARTIFACTORY_TOKEN) {
 const main = async () => {
   try {
     await exec.exec("curl", ["-f", "-s", "-H", token, "-O", artifactoryURL]);
-=======
-
-const token = "Authorization: Bearer " + process.env.ARTIFACTORY_TOKEN;
-const toolsPath = process.env.RUNNER_TOOL_CACHE;
-
-const main = async () => {
-  try {
-    await exec.exec("curl", [
-      "-f",
-      "-H",
-      token,
-      "-O",
-      "https://artifactory-de.asml.com/artifactory/rise-generic-dev-local/toolchain-cache/go_tool_cache.tar.gz",
-    ]);
-
->>>>>>> 50afeb71386027d635b32a331604a6110df1b5d0
     await exec.exec("tar", ["-zxf", "go_tool_cache.tar.gz"]);
     await exec.exec("cp", ["-r", "go", toolsPath]);
   } catch (error) {
     core.setFailed(error.message);
   }
-<<<<<<< HEAD
 
   const directories = getDirectories(toolsPath + "/go/");
   let toolsDirVersion;
@@ -3045,33 +3026,6 @@ const main = async () => {
       "go-version needs to be specified !! (SUPPORTED VERSIONS:1.14.x, 1.15.x, 1.16.x, 1.17.x"
     );
     return 1;
-=======
-  try {
-    let version = core.getInput("go-version");
-    await exec.exec("echo", [version]);
-
-    switch (version.slice(0, 4)) {
-      case "1.14":
-        core.addPath(toolsPath+"/go/1.14.15/x64/bin");
-        break;
-      case "1.15":
-        core.addPath(toolsPath+"/go/1.15.15/x64/bin");
-        break;
-      case "1.16":
-        core.addPath(toolsPath+"/go/1.16.15/x64/bin");
-        break;
-      case "1.17":
-        core.addPath(toolsPath+"/go/1.17.9/x64/bin");
-        break;
-      default:
-        core.setFailed(
-          "go-version needs to be specified !! (SUPPORTED VERSIONS:1.14.x, 1.15.x, 1.16.x, 1.17.x)"
-        );
-        break;
-    }
-  } catch (error) {
-    core.setFailed(error.message);
->>>>>>> 50afeb71386027d635b32a331604a6110df1b5d0
   }
 };
 
