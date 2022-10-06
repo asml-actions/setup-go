d730d4875a3e597ab401bc16147b81a8a871ed2b
Hindrik Bruinsma <hindrik.bruinsma@asml.com>
2022-10-06 10:28:03 +0200

    Cleanup old tags when present

--- update-tags.sh
+++ update-tags.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+declare -a OLD_TAGS=("v1" "v1.0.0" "v1.0.1" "v1.0.2" "v1.1.0" "v1.1.1" "v1.1.2" "v2" "v2.0.1" "v2.0.2" "v2.0.3" "v2.1.0" "v2.1.1" "v2.1.2" "v2.1.3" "v2.1.4" "v2.1.5" "v2.2.0" "v3" "v3.0.0")
 declare -a TAGS=("v1" "v2" "v3")
 
-for TAG in ${TAGS[@]}; do
+for TAG in ${OLD_TAGS[@]}; do
     git tag -d $TAG
     git push --delete origin $TAG
+    git push --delete prd $TAG
     git push --delete acc $TAG
+done
+
+for TAG in ${TAGS[@]}; do
     git tag $TAG
-    git push --tags -u origin
-    git push --tags -u acc
+    git push --tags -u origin -f
+    git push --tags -u prd -f
+    git push --tags -u acc -f
 done
